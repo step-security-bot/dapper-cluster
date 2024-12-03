2d05ee089028319b63961ace46ff990f4ad5f80f
Derek Mackley <derek.mackley@hotmail.com>
2024-12-03 10:25:26 -0600

    remove external secret from kubeconform

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ReplicationDestination,ReplicationSource"
+    "Secret,ReplicationDestination,ReplicationSource,ExternalSecret"
     "-schema-location"
     "default"
     "-schema-location"
